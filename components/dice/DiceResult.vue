88c81da0eebec5de0f9c946787340fd07ce05ebe
unknown <you@example.com>
2019-11-01 09:17:32 +0300

    Добавлена SVG-маска на соты

--- components/dice/DiceResult.vue
+++ components/dice/DiceResult.vue
@@ -1,27 +1,42 @@
 <template>
   <div class="dice-result">
     <div class="dice-result-item">
-      <div class="dice-result-item-value">
-        {{ result.charAt(0) }}
+      <div class="clip-wrap">
+        <div class="dice-result-item-value">
+          {{ result.charAt(0) }}
+        </div>
       </div>
     </div>
     <div class="dice-result-item">
-      <div class="dice-result-item-value">
-        {{ result.charAt(1) }}
+      <div class="clip-wrap">
+        <div class="dice-result-item-value">
+          {{ result.charAt(1) }}
+        </div>
       </div>
     </div>
     <div class="dice-result-item">
-      <div class="dice-result-item-value">
-        {{ result.charAt(2) }}
+      <div class="clip-wrap">
+        <div class="dice-result-item-value">
+          {{ result.charAt(2) }}
+        </div>
       </div>
     </div>
     <div class="dice-result-item">
-      <div class="dice-result-item-value">
-        {{ result.charAt(3) }}
+      <div class="clip-wrap">
+        <div class="dice-result-item-value">
+          {{ result.charAt(3) }}
+        </div>
       </div>
     </div>
     <div class="clearfix" />
     <div class="dice-result-dot" />
+    <svg class="clip-svg">
+      <defs>
+        <clipPath id="clip-svg" clipPathUnits="objectBoundingBox">
+          <polygon points="0.5 0, 1 0.33, 1 0.66, 0.5 1, 0 0.66, 0 0.33" />
+        </clipPath>
+      </defs>
+    </svg>
   </div>
 </template>
 
@@ -69,7 +84,7 @@ export default {
   width: 117px;
   height: 134px;
   margin-right: 12px;
-  background: url('~assets/honeycomb.png') no-repeat transparent;
+  background: url('~assets/honeycomb_yellow.png') no-repeat transparent;
   float: left;
 }
 
@@ -77,12 +92,22 @@ export default {
   margin-right: 0;
 }
 
+.clip-wrap {
+  width: 101px;
+  height: 107px;
+  margin-top: 14px;
+  margin-left: 8px;
+  -webkit-clip-path: url("#clip-svg");
+  clip-path: url("#clip-svg");
+}
+
 .dice-result-item-value {
+  position: relative;
   width: 35px;
   height: 66px;
   font-size: 65px;
   color: #fff;
-  margin: 33px 0 0 42px;
+  margin: 33px 0 0 34px;
   line-height: 1;
 }
 
@@ -99,5 +124,4 @@ export default {
 .clearfix {
   clear: both;
 }
-
 </style>
